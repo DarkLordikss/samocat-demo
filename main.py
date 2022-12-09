f5082bfdb43407638ad57fada1738852305a3042
DarkLordikss <75681846+DarkLordikss@users.noreply.github.com>
2022-12-09 23:25:57 +0700

    one more naming fix

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def samokat():
+def firstPage():
     return render_template('index.html')
 
 @app.route("/second_page")
-def obut():
+def secondPage():
     return render_template('3-2.html')
 
 if __name__ == "__main__":
