1f05b88698b780d55bcf39657d153685c42cf304
DarkLordikss <75681846+DarkLordikss@users.noreply.github.com>
2022-12-09 23:27:23 +0700

    codestyle fix

--- main.py
+++ main.py
@@ -3,13 +3,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
-def firstPage():
+def first_page():
     return render_template('index.html')
 
+
 @app.route("/second_page")
-def secondPage():
+def second_page():
     return render_template('3-2.html')
+
 
 if __name__ == "__main__":
     app.run()
