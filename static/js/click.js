0a86810bd65d44c856283413995cd4f9ac93f6e5
DarkLordikss <75681846+DarkLordikss@users.noreply.github.com>
2022-12-11 09:21:32 +0700

    js bug fixed

--- static/js/click.js
+++ static/js/click.js
@@ -1,15 +1,30 @@
+var k1 = 0;
+var k2 = 0;
+var k3 = 0;
 function buttonGreen1() {
     var element = document.getElementById("b1");
     element.classList.add("green");
-    element.textContent = "Подтверждено"
+    element.textContent = "Подтверждено";
+    k1 = 1;
+    if (k1 == 1 && k2 == 1 && k3 == 1) {
+        window.location.href = '/promo';
+    }
 }
 function buttonGreen2() {
     var element = document.getElementById("b2");
     element.classList.add("green");
-    element.textContent = "Подтверждено"
+    element.textContent = "Подтверждено";
+    k2 = 1;
+    if (k1 == 1 && k2 == 1 && k3 == 1) {
+        window.location.href = '/promo';
+    }
 }
 function buttonGreen3() {
     var element = document.getElementById("b3");
     element.classList.add("green");
-    element.textContent = "Подтверждено"
+    element.textContent = "Подтверждено";
+    k3 = 1;
+    if (k1 == 1 && k2 == 1 && k3 == 1) {
+        window.location.href = '/promo';
+    }
 }
